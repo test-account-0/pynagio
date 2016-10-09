9cab951a8cd490f89eb8ca22020f15f9166565c8
Paweł Jarmoń <traszakaunt@gmail.com>
2016-10-09 23:47:12 +0200

    version bump to 0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pynagio',
-    version='0.1.5',
+    version='0.1.6',
     description="Super simple nagios check python library",
     packages=['pynagio'],
 )
